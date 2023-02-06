1e36ec276bcb9c5522de1fa0a59c7b459eda4df7
DagoGres <dan333pro@gmai.com>
2023-02-06 12:11:37 -0500

    Head modified

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <meta charSet='utf-8' />
+      </Head>
       <body>
         <Main />
         <NextScript />
