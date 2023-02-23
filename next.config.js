dc16996f5b2e70ce8c00d6c3095141adbcea68be
DagoGres <dan333pro@gmai.com>
2023-02-23 13:21:12 -0500

    PWA y ico

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa') ({
+  dest: 'public',
+  include: ['production'],
+  register: true,
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
