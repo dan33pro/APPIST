c28157a661b42fb8b80dfc8ad6b052d3d4cc51a0
DagoGres <dan333pro@gmai.com>
2023-03-02 20:13:32 -0500

    Optimización de imagnes 1

--- src/containers/CardsContainer.jsx
+++ src/containers/CardsContainer.jsx
@@ -1,9 +1,9 @@
 import styles from '@styles/CardsContainer.module.scss';
 import Card from '@components/Card';
 
-import imgDoc from '@images/documentos.jpg';
-import imgProj from '@images/proyecto.jpg';
-import imgTeam from '@images/equipo.jpg';
+import imgDoc from '@images/documentos-min.jpg';
+import imgProj from '@images/proyecto-min.jpg';
+import imgTeam from '@images/equipo-min.jpg';
 
 const cardOne = {
   id: 1,
